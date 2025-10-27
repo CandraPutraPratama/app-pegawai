48156b02c8f174ec569b5ae12629044d0d2ca976
CandraPutraPratama <candraputrap46@gmail.com>
2025-10-28 01:03:33 +0700

    Penambahan penambahan CSS fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,14 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-// use App\Models\User; // Bisa dihapus karena kita gak pake factory
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // 👇 Panggil Seeder yang lo butuhkan di sini! 👇
         $this->call([
-            // Department harus duluan karena Position mungkin butuh Departemen (walaupun gak di case ini)
             DepartmentSeeder::class,
             PositionSeeder::class,
-            // Kalo mau buat user baru yang aman, bisa taruh di sini, tapi jangan pakai 'test@example.com'
         ]);
     }
 }
